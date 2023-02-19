67659972b6012d05c66f7aa78a7676eb46c5b507
Diyana Cholakova <didkacholakova999@gmail.com>
2023-02-19 21:45:07 +0200

    Ado.Net Exercise

---
+++ C#/C#-DB/02. Entity Framework Core/01. ADO.NET - Exercise/Exercise/AdoNetExercise/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace AdoNetExercise
+{
+    internal static class Config
+    {
+        public const string ConnectionString =
+            @"Server=.;Database=MinionsDB;Integrated Security=True;Trust Server Certificate=True";
+    }
+}
