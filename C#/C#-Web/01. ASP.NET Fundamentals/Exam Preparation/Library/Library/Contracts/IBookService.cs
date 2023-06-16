dbe317dec2c439249b32d8a2df814e21f8bf910c
Diyana Cholakova <didkacholakova999@gmail.com>
2023-06-16 11:25:20 +0300

    C# -> ASP.NET Fund -> Exam Prep

---
+++ C#/C#-Web/01. ASP.NET Fundamentals/Exam Preparation/Library/Library/Contracts/IBookService.cs
@@ -0,0 +1,21 @@
+﻿namespace Library.Contracts
+{
+    using Models;
+
+    public interface IBookService
+    {
+        Task AddBookToCollectionAsync(string userId, BookViewModel book);
+        
+        Task RemoveFromCollectionAsync(string userId, BookViewModel book);
+        
+        Task<IEnumerable<AllBookViewModel>> GetAllBooksAsync();
+        
+        Task<BookViewModel?> GetBookByIdAsync(int id);
+
+        Task<IEnumerable<AllBookViewModel>> GetMyBooksAsync(string userId);
+        
+        Task<AddBookViewModel> GetNewAddBookModelAsync();
+        
+        Task AddBookAsync(AddBookViewModel model);
+    }
+}
---
+++ C#/C#-Web/01. ASP.NET Fundamentals/Exam Preparation/Library/Library/Data/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿namespace Library.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    
+    using Microsoft.EntityFrameworkCore;
+
+    [Comment("Categories for the books")]
+    public class Category
+    {
+        [Comment("Primary key")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Name of the category")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = null!;
+
+        public List<Book> Books { get; set; } = new List<Book>();
+    }
+}
